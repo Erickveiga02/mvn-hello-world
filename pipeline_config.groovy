14d5b092edb4414b5475f1ecdb1a3d3b9165e1b1
Erick da Silva Veiga <erick.veiga.ext@sascar.com.br>
2023-02-01 14:49:38 -0300

    update

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,4 +1,10 @@
-pipeline_template = 'maven-pipeline-sast'
+jte{ 
+    allow_scm_jenkinsfile = true
+    pipeline_template = "maven-pipeline-sast"
+    permissive_initialization = false
+    reverse_library_resolution = false
+}
+
 libraries{
     maven
     
